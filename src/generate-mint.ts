a249efb402067d35a0623f8bfa2fdb70dba88add
Dipu <dipuchaurasiya91@gmail.com>
2024-12-21 06:22:11 +0545

    feat: Add Zoho Desk to provider guides

--- src/generate-mint.ts
+++ src/generate-mint.ts
@@ -257,6 +257,7 @@ const mintConfig: MintConfig = {
             "provider-guides/wrike",
             "provider-guides/zendeskSupport",
             "provider-guides/zoho",
+            "provider-guides/zohoDesk",
             "provider-guides/zoom"
           ],
         },
