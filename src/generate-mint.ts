7b39cea7f5d949b7980be162eb8c0b91fabe12c1
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-01-16 18:28:18 +0530

    [CON-612] feat: Added the provider guides for groove (#149)
    * [CON-612] feat: Added the provider guides for groove
    
    * Update src/provider-guides/groove.mdx
    
    ---------
    
    Co-authored-by: Rajat Pawar <18614743+RajatPawar@users.noreply.github.com>

--- src/generate-mint.ts
+++ src/generate-mint.ts
@@ -211,6 +211,7 @@ const mintConfig: MintConfig = {
             "provider-guides/google",
             "provider-guides/gorgias",
             "provider-guides/goToWebinar",
+            "provider-guides/groove",
             "provider-guides/guru",
             "provider-guides/helpScoutMailbox",
             "provider-guides/heyreach",
