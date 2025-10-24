755f2efb1cc4ceee17befb40ae002ffddde44736
Sanjay A <163011126+sanjay-a01@users.noreply.github.com>
2025-10-24 11:51:20 +0530

    [CON-2035] feat: [loxo] Added the provider guide (#326)

--- src/generate-docs.ts
+++ src/generate-docs.ts
@@ -372,6 +372,7 @@ const baseConfig = {
             "provider-guides/linear",
             "provider-guides/linkedin",
             "provider-guides/livestorm",
+            "provider-guides/loxo",
             "provider-guides/mailgun",
             "provider-guides/marketo",
             "provider-guides/meta",
