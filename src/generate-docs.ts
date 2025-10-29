8b76118eb68c827c636169a79c05d120968c86c3
Lauren Long <laurenzlong@users.noreply.github.com>
2025-10-28 17:23:12 -0700

    update subscribe actions, Chorus provider guide, and hide Greenhouse Job Board (#391)

--- src/generate-docs.ts
+++ src/generate-docs.ts
@@ -346,7 +346,7 @@ const baseConfig = {
             "provider-guides/gong",
             "provider-guides/google",
             "provider-guides/gorgias",
-            "provider-guides/greenhouseJobBoard",
+            // "provider-guides/greenhouseJobBoard",
             "provider-guides/groove",
             "provider-guides/guru",
             "provider-guides/happyfox",
