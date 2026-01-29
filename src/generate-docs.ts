d746f74f46fbe10a91e627d2cd0701a08e59fc22
Joseph Karage <josephbkarage@gmail.com>
2026-01-29 08:44:17 +0300

    [CON-2631] feat: Add solarwinds provider guides (#461)
    * Add solarwinds provider guides
    
    * Update src/provider-guides/solarwindsServiceDesk.mdx
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    * Update src/provider-guides/solarwindsServiceDesk.mdx
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- src/generate-docs.ts
+++ src/generate-docs.ts
@@ -438,6 +438,7 @@ const baseConfig = {
             "provider-guides/smartsheet",
             "provider-guides/snapchatAds",
             "provider-guides/snowflake",
+            "provider-guides/solarwindsServiceDesk",
             "provider-guides/stripe",
             "provider-guides/surveyMonkey",
             "provider-guides/talkdesk",
