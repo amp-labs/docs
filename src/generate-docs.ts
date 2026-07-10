d9a27df7fa6c6a61cce2869583495b00b3361134
Yafet Yohaness <85060550+Lafetz@users.noreply.github.com>
2026-07-10 20:15:46 +0300

    [CON-3339] feat(jump): Create guide on amp-labs/docs (#638)

--- src/generate-docs.ts
+++ src/generate-docs.ts
@@ -410,6 +410,7 @@ const baseConfig = {
             "provider-guides/jobber",
             "provider-guides/joinMe",
             "provider-guides/jotform",
+            "provider-guides/jump",
             "provider-guides/justCall",
             "provider-guides/kaseyaVSAX",
             "provider-guides/keap",
