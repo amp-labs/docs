141a0c5494bb08485e3d426da4832239a561dfec
Constantin Koval <60330780+Cobalt0s@users.noreply.github.com>
2026-08-28 04:00:07 +0300

    [CON-3495,CON-3507] feat(lob): New provider guide (#712)

--- src/generate-docs.ts
+++ src/generate-docs.ts
@@ -446,6 +446,7 @@ const baseConfig = {
             "provider-guides/linear",
             "provider-guides/linkedin",
             "provider-guides/livestorm",
+            "provider-guides/lob",
             "provider-guides/loxo",
             "provider-guides/mailgun",
             "provider-guides/marketo",
